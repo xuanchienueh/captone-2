2845d3164b707222b0cd37aa137aada72173be6e
xuanchienueh <xuanchien.ueh@gmail.com>
2022-07-14 06:54:07 +0700

    create branch

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import demSoReducer from "../counter/demSoSlice";
 
 export const store = configureStore({
   reducer: {
     //reducer: ItemReducer
+    demSo: demSoReducer,
   },
 });
