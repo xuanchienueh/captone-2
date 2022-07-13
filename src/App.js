67ee5e7d31c3c996aa25d843b179bf2862c20177
xuanchienueh <xuanchien.ueh@gmail.com>
2022-07-13 08:19:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { getDSFilm, giam, tang } from "./counter/demSoSlice";
 
 function App() {
-  const dispatch = useDispatch();
-  const { value1, loaiNguoiDung } = useSelector((state) => state.demSo);
-  console.log(loaiNguoiDung);
-
-  useEffect(() => {
-    dispatch(getDSFilm());
-  }, [value1]);
-
   return (
     <div className="App">
-      <h1>Redux Toolkit</h1>
-      <h2>{value1}</h2>
-      <button
-        onClick={() => {
-          dispatch(tang());
-        }}
-      >
-        tang len
-      </button>
-      <button onClick={() => dispatch(giam())}>giam</button>
+      <h1>Captone 2</h1>
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import demSoReducer from "../counter/demSoSlice";
 
 export const store = configureStore({
   reducer: {
-    demSo: demSoReducer,
+    //reducer: ItemReducer
   },
 });
