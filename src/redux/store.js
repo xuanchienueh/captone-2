5796f4e023058fda0037af05816f45c47901fc5d
Nguyễn Xuân Chiến <97378366+xuanchienueh@users.noreply.github.com>
2022-07-14 07:25:04 +0700

    Revert " create branch"

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import demSoReducer from "../counter/demSoSlice";
 
 export const store = configureStore({
   reducer: {
     //reducer: ItemReducer
-    demSo: demSoReducer,
   },
 });
